35377d7ba9d09472ba2c30f207e062da7e578f51
shahed2shahed <shahedalhomsi77@gmail.com>
2025-12-18 14:46:18 +0300

    update function

--- Modules/Accounts/Services/Admin/AdminService.php
+++ Modules/Accounts/Services/Admin/AdminService.php
@@ -6,7 +6,7 @@ use Modules\Accounts\Entities\Account;
 use Modules\Accounts\Services\Account\AccountFactory;
 use Modules\Accounts\Services\Account\ApproveRejectInterface;
 
-
+use Illuminate\Support\Carbon;
 
 class AdminService
 {
@@ -22,7 +22,7 @@ class AdminService
 
         $service->approve($account);
         $message = 'Account approved successfully';
-        return ['account' => $account , 'message' => $message];  
+        return ['account' => $account , 'message' => $message];
     }
 
     public function reject(Account $account, array $data): void
@@ -34,5 +34,78 @@ class AdminService
         }
 
         $service->reject($account, $data);
+    }
+
+
+    public function updateChecnindSetting(Request $request, $id): array{
+    $checking = CheckingAccount::findOrFail($id);
+    $validatedData = $request->validate([
+        'minimum_balance' => 'sometimes|numeric',
+        'overdraft_limit' => 'sometimes|numeric',
+        'overdraft_fees' => 'sometimes|numeric',
+        'monthly_fees' => 'sometimes|numeric',
+        'year_version'  => 'sometimes',
+    ]);
+
+    $checking->update($validatedData);
+    $checking->year_version = now()->year;
+
+
+        $message = 'checking updated successfully';
+        return ['checking' => $checking , 'message' => $message];
+    }
+
+    public function updateLoanSetting(Request $request, $id): array{
+    $loan = LoanAccount::findOrFail($id);
+    $validatedData = $request->validate([
+        'interest_rate' => 'sometimes|numeric',
+        'interest_rate_type' => 'sometimes|numeric',
+        'late_payment_fees' => 'sometimes|numeric',
+        'processing_fees' => 'sometimes|numeric',
+        'max_tenure_months' => 'sometimes|numeric',
+        'min_loan_amount' => 'sometimes|numeric',
+        'max_loan_amount' => 'sometimes|numeric',
+    ]);
+
+    $loan->update($validatedData);
+    $loan->year_version = now()->year;
+
+        $message = 'loan updated successfully';
+        return ['loan' => $loan , 'message' => $message];
+    }
+
+
+    public function updateSavingsSetting(Request $request, $id): array{
+    $saving = SavingsAccount::findOrFail($id);
+    $validatedData = $request->validate([
+
+        'interest_rate'  => 'sometimes|numeric',
+        'minimumbalancefor_interest'  => 'sometimes|numeric',
+        'freewithdrawlimitpermonth'  => 'sometimes|numeric',
+        'withdrawfeeafter_limit'  => 'sometimes|numeric',
+    ]);
+
+    $saving->update($validatedData);
+    $saving->year_version = now()->year;
+
+        $message = 'saving updated successfully';
+        return ['saving' => $saving , 'message' => $message];
+    }
+
+
+        public function updateInvesmentSetting(Request $request, $id): array{
+    $invest = InvestmentAccount::findOrFail($id);
+    $validatedData = $request->validate([
+        'expected_returns'  => 'sometimes|numeric',
+        'minimum_investment'  => 'sometimes|numeric',
+        'managementfeespercentage'  => 'sometimes|numeric',
+
+    ]);
+
+    $invest->update($validatedData);
+    $invest->year_version = now()->year;
+
+        $message = 'invest updated successfully';
+        return ['invest' => $invest , 'message' => $message];
     }
 }
