56039abd3af1bf9269fde58b67a495588027f1e8
shahed2shahed <shahedalhomsi77@gmail.com>
2025-12-19 19:37:22 +0300

    edit

--- Modules/Transaction/Services/TransitionService.php
+++ Modules/Transaction/Services/TransitionService.php
@@ -18,8 +18,40 @@ public function __construct( private TransitionStrategy $transitionStrategy){
         $statusService = $this->transitionStrategy->withdrawStr($account->$account_status_id);
         $resultData=[];
         $resultData=$statusService->withdraw( $account , $request);
+        DB::commit();
 
+        return  $resultData;
 
+        } catch (Throwable $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+        public function deposit($accountId , $request): array{
+        DB::beginTransaction();
+        try{
+        $account = Account::find($accountId);
+        $statusService = $this->transitionStrategy->depositStr($account->$account_status_id);
+        $resultData=[];
+        $resultData=$statusService->deposit( $account , $request);
+        DB::commit();
+
+        return  $resultData;
+
+        } catch (Throwable $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+            public function transfer($accountId , $request): array{
+        DB::beginTransaction();
+        try{
+        $account = Account::find($accountId);
+        $statusService = $this->transitionStrategy->transferStr($account->$account_status_id);
+        $resultData=[];
+        $resultData=$statusService->transfer( $account , $request);
         DB::commit();
 
         return  $resultData;
