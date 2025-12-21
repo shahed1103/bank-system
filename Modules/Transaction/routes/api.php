d686bb0e672272b596a8c8e8543fde1c731f4205
shahed2shahed <shahedalhomsi77@gmail.com>
2025-12-21 19:13:47 +0300

    api

--- Modules/Transaction/routes/api.php
+++ Modules/Transaction/routes/api.php
@@ -8,4 +8,17 @@ Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
 });
 
 
-Route::get('suspend/{accountId}', [AdminController::class, 'suspend']);
+Route::get('suspend/{accountId}', [TransactionController::class, 'suspend']);
+
+Route::post('withdraw/{accountId}', [TransactionController::class, 'withdraw']);
+Route::post('deposit/{accountId}', [TransactionController::class, 'deposit']);
+Route::post('transfer/{accountId}', [TransactionController::class, 'transfer']);
+Route::post('getNonApprovedTransition', [TransactionController::class, 'getNonApprovedTransition']);
+Route::post('getNonApprovedTransfer', [TransactionController::class, 'getNonApprovedTransfer']);
+Route::post('approveTransition/{tranId}', [TransactionController::class, 'approveTransition']);
+Route::post('approveTransfer/{tranId}', [TransactionController::class, 'approveTransfer']);
+Route::post('getTransHistory/{tranId}', [TransactionController::class, 'getTransHistory']);
+
+
+
+
