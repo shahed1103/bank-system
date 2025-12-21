412d6d2cf1fd967c54240a94da63126eed7363d7
shahed1103 <raniasol666@gmail.com>
2025-12-21 14:52:46 +0300

    Update api.php

--- Modules/CustomerService/routes/api.php
+++ Modules/CustomerService/routes/api.php
@@ -5,15 +5,15 @@ use Modules\CustomerService\Http\Controllers\CustomerServiceController;
 use Modules\CustomerService\Http\Controllers\TicketController;
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('createTicket', [TicketController::class, 'createTicket']);
-    Route::get('getUserTickets', [TicketController::class, 'getUserTickets']);
-    Route::get('getTicketDetails/{ticketId}', [TicketController::class, 'getTicketDetails']);
-    Route::post('reply/{ticketId}', [TicketController::class, 'reply']);
-    Route::post('changeStatus/{ticketId}', [TicketController::class, 'changeStatus']);
+    Route::post('createTicket', [TicketController::class, 'createTicket'])->middleware('can:createTicket');
+    Route::get('getUserTickets', [TicketController::class, 'getUserTickets'])->middleware('can:getUserTickets');
+    Route::get('getTicketDetails/{ticketId}', [TicketController::class, 'getTicketDetails'])->middleware('can:getTicketDetails');
+    Route::post('reply/{ticketId}', [TicketController::class, 'reply'])->middleware('can:reply');
+    Route::post('changeStatus/{ticketId}', [TicketController::class, 'changeStatus'])->middleware('can:changeStatus');
 });
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('getRecommendations', [CustomerServiceController::class, 'getRecommendations']);
+    Route::get('getRecommendations', [CustomerServiceController::class, 'getRecommendations'])->middleware('can:getRecommendations');
 });
 
